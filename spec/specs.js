0096d09de2205a6437482afedea118f299040f25
David Zulaica and Mike Goren <zulaica@gmail.com>
2015-02-16 09:51:42 -0800

    create js addressbook

--- spec/specs.js
+++ spec/specs.js
@@ -1,4 +1,8 @@
 describe('addressBook', function() {
-
-
+  //
+  // it("creates an object that stores a person's first and last names, and their address", function() {
+  //   var newContact = { firstName: "John", lastName: "Doe", address: "742 Evergreen Terrace" };
+  //   expect(newContact.firstName).to.equal("John");
+  // });
+  //
 });
